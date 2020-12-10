68b20aeaa29dd7e5fadecbea93ec6f11e0531eaa
AbDev <ab.inpathtoadev@gmail.com>
2020-12-10 12:57:01 +0530

    v3 for heroku attempt1

--- hello.py
+++ hello.py
@@ -51,3 +51,7 @@ def change_nav():
 
     if request.method == 'GET':
         return redirect('/')
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
